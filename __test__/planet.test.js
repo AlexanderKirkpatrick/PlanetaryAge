622a3ade500aa759aaa4b7788299ef9539a12346
Alexander <knibz@protonmail.com>
2022-05-19 23:01:08 -0700

    edit red workflow planets test to include average

--- __test__/planet.test.js
+++ __test__/planet.test.js
@@ -3,7 +3,7 @@ import { Planets } from "../src/planet";
 describe('Planets', () => {
   let newPlanets;
   beforeEach(() => {
-    newPlanets = new Planets(4.14, 1.62, .53, .08)
+    newPlanets = new Planets(4.14, 1.62, .53, .08, 74)
   });
 
   test('should create a planets object with difference value', () => {
@@ -12,6 +12,7 @@ describe('Planets', () => {
     expect(newPlanets.venus).toEqual(1.62);
     expect(newPlanets.mars).toEqual(.53);
     expect(newPlanets.jupiter).toEqual(.08);
+    expect(newPlanets.average).toEqual(74);
   });
 
 });
