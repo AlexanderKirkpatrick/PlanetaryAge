076461b338c2c2f0100b7c9a6f6ace8ead5dacc1
Alexander <knibz@protonmail.com>
2022-05-19 23:04:09 -0700

    add class, constructor that passes test, fulfilling green phase workflow

--- src/planet.js
+++ src/planet.js
@@ -1,3 +1,9 @@
 export class Planets {
-  
+  constructor (mercury, venus, mars, jupiter, average) {
+    this.mercury = mercury;
+    this.venus = venus;
+    this.mars = mars;
+    this.jupiter = jupiter;
+    this.average = 74;
+  }
 }
