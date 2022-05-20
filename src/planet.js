b161e093aca958e1e3af49b22729314d35c6b696
Alexander <knibz@protonmail.com>
2022-05-20 00:38:34 -0700

    add passing ageOnMercury function

--- src/planet.js
+++ src/planet.js
@@ -1,9 +1,14 @@
 export class Planets {
-  constructor (mercury, venus, mars, jupiter, average) {
-    this.mercury = mercury;
-    this.venus = venus;
-    this.mars = mars;
-    this.jupiter = jupiter;
+  constructor (mercury, venus, mars, jupiter, average, age) {
+    this.mercury = 4.14;
+    this.venus = 1.62;
+    this.mars = .53;
+    this.jupiter = .08;
     this.average = 74;
+    this.age = 0;
+  }
+
+  ageOnMercury(number) {
+   this.age = (this.mercury) * number;
   }
 }
