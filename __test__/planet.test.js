46cc5b9fe51de993490563eb64fd76ec312e83ab
Alexander <knibz@protonmail.com>
2022-05-20 00:37:02 -0700

    edit tests to obtain 100% line coverage

--- __test__/planet.test.js
+++ __test__/planet.test.js
@@ -1,20 +1,24 @@
 import { Planets } from "../src/planet";
 
 describe('Planets', () => {
-  
+  let newPlanets;
+  beforeEach(() => {
+    newPlanets = new Planets(4.14, 1.62, .53, .08, 74, 0);
+  });
+   
+
   test('should create a planets object with difference value', () => {
-    let newPlanets = new Planets(4.14, 1.62, .53, .08, 74)
     expect(newPlanets.mercury).toEqual(4.14);
     expect(newPlanets.venus).toEqual(1.62);
     expect(newPlanets.mars).toEqual(.53);
     expect(newPlanets.jupiter).toEqual(.08);
     expect(newPlanets.average).toEqual(74);
+    expect(newPlanets.age).toEqual(0);
   });
 
   test('should correctly determine the users age on mercury', () => {
-    const age = 37;
-    const mercury = 4.14;
-    expect(age.mercury).toEqual(153.18);
+    newPlanets.ageOnMercury(37)
+    expect(newPlanets.age).toEqual(153.17999999999998);
   });
 
 });
