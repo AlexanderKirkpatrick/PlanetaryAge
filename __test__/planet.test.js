2dfdac08339e276a0b50036c59aac158aa077759
Alexander <knibz@protonmail.com>
2022-05-19 23:10:21 -0700

    refactor test to remove three lines while retaining 100% coverage, fulfilling refactor workflow

--- __test__/planet.test.js
+++ __test__/planet.test.js
@@ -1,13 +1,9 @@
 import { Planets } from "../src/planet";
 
 describe('Planets', () => {
-  let newPlanets;
-  beforeEach(() => {
-    newPlanets = new Planets(4.14, 1.62, .53, .08, 74)
-  });
-
+  
   test('should create a planets object with difference value', () => {
-
+    newPlanets = new Planets(4.14, 1.62, .53, .08, 74)
     expect(newPlanets.mercury).toEqual(4.14);
     expect(newPlanets.venus).toEqual(1.62);
     expect(newPlanets.mars).toEqual(.53);
