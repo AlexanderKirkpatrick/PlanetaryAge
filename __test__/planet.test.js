2a7aee51c8829b8bb3907f400bf3ab1fdf181aac
Alexander <knibz@protonmail.com>
2022-05-19 23:17:25 -0700

    add test to determine the users age on mercury

--- __test__/planet.test.js
+++ __test__/planet.test.js
@@ -3,12 +3,18 @@ import { Planets } from "../src/planet";
 describe('Planets', () => {
   
   test('should create a planets object with difference value', () => {
-    newPlanets = new Planets(4.14, 1.62, .53, .08, 74)
+    let newPlanets = new Planets(4.14, 1.62, .53, .08, 74)
     expect(newPlanets.mercury).toEqual(4.14);
     expect(newPlanets.venus).toEqual(1.62);
     expect(newPlanets.mars).toEqual(.53);
     expect(newPlanets.jupiter).toEqual(.08);
     expect(newPlanets.average).toEqual(74);
+  });
+
+  test('should correctly determine the users age on mercury', () => {
+    const age = 37;
+    const mercury = 4.14;
+    expect(age.mercury).toEqual(153.18);
   });
 
 });
