0a7dbc71dd1a9f47eb36a196e320ef75cfcdf161
Alexander <knibz@protonmail.com>
2022-05-19 22:45:46 -0700

    add red workflow planets test

--- __test__/planet.test.js
+++ __test__/planet.test.js
@@ -1,1 +1,17 @@
 import { Planets } from "../src/planet";
+
+describe('Planets', () => {
+  let newPlanets;
+  beforeEach(() => {
+    newPlanets = new Planets(4.14, 1.62, .53, .08)
+  });
+
+  test('should create a planets object with difference value', () => {
+
+    expect(newPlanets.mercury).toEqual(4.14);
+    expect(newPlanets.venus).toEqual(1.62);
+    expect(newPlanets.mars).toEqual(.53);
+    expect(newPlanets.jupiter).toEqual(.08);
+  });
+
+});
