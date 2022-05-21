27d1790507ae4e4a10d677782799310054d2be05
Alexander <knibz@protonmail.com>
2022-05-21 13:02:46 -0700

    add passing mortality class

--- src/mortality.js
+++ src/mortality.js
@@ -1,5 +1,9 @@
 export class Mortality {
-  constructor (mercuryLife, venusLife, marsLife, jupiterLife) {
+  constructor (mercuryLife, venusLife, marsLife, jupiterLife, remainder) {
     this.mercuryLife = 306.35999999999996;
+    this.venusLife = 119.88000000000001;
+    this.marsLife = 39.22;
+    this.jupiterLife = 5.92;
+    this.remainder = 0
   }
 }
