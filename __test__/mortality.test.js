820c6fd1225e3cdd7b9e13678bc732a23f845e81
Alexander <knibz@protonmail.com>
2022-05-21 12:58:54 -0700

    add test for mortality object

--- __test__/mortality.test.js
+++ __test__/mortality.test.js
@@ -1,1 +1,18 @@
 import { Mortality } from "../src/mortality";
+
+describe('Mortality', () => {
+  let newMortality;
+  beforeEach(() => {
+    newMortality = new Mortality();
+  });
+   
+
+  test('should create a planets object with difference value', () => {
+    expect(newMortality.mercuryLife).toEqual(306.35999999999996);
+    expect(newMortality.venusLife).toEqual(119.88000000000001);
+    expect(newMortality.marsLife).toEqual(39.22);
+    expect(newMortality.jupiterLife).toEqual(5.92);
+    expect(newMortality.remainder).toEqual(0);
+  });
+
+});
