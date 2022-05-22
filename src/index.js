05a4f63a06c7872a3743c96f37ca11e0c20b6dba
Alexander <knibz@protonmail.com>
2022-05-21 23:06:15 -0700

    final commit before branch creation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
+import './styles.css';
+import $ from 'jquery';
+
+//import { Planets } from "../src/planet";
+//import { Mortality } from "../src/mortality";
+
+$(document).ready(function() {
+  $('#user-input-form').submit(function(event) {
+    event.preventDefault();
+    // const userAge = $('#user-age').val();
+    
+
+    // $("#mercAge") = userAge.ageOnMercury;
+
+    
+
+    $('#newAge').show();
+
+  });
+
+});
